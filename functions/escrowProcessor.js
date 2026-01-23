499f90def4afe9c235fd17fddb3a863abc3a4d78
Michael Chandra <michaelchandra@Michaels-MacBook-Air.local>
2026-01-23 14:42:17 +0700

    fixing obviously stupid mistake

--- functions/escrowProcessor.js
+++ functions/escrowProcessor.js
@@ -15,53 +15,54 @@ export async function fetchDanaDilepas(brand, partner_id, partner_key, access_to
     const HOST = "https://partner.shopeemobile.com";
     const PATH = "/api/v2/payment/get_escrow_list";
 
-    const timestamp = Math.floor(Date.now() / 1000);
-    const baseString = `${partner_id}${PATH}${timestamp}${access_token}${shop_id}`;
-    const sign = crypto.createHmac('sha256', partner_key)
-        .update(baseString)
-        .digest('hex');
-
-    const releaseTimeStart = Math.floor(new Date("2026-01-01") / 1000);
-    const releaseTimeEnd = Math.floor(new Date("2026-01-03") / 1000);
-    
-    console.log("Release time start: ", releaseTimeStart);
-    console.log("Release time end: ", releaseTimeEnd);
-
-    const params = new URLSearchParams({
-        partner_id: partner_id,
-        timestamp,
-        access_token: access_token,
-        shop_id: shop_id,
-        sign,
-        release_time_from: releaseTimeStart,
-        release_time_to: releaseTimeEnd
-    });
-
-    const fullUrl = `${HOST}${PATH}?${params.toString()}`;
-    console.log(`Hitting Dana Dilepas for ${brand}: `, fullUrl);
-
     try {
-        const response = await axios.get(fullUrl, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
     
         console.log("[SHOPEE-WITHDRAWAL] Raw response for brand: ", brand);
         
         let danaDilepas = []
         let count = 0;
-        while(response.data.response.more) {
-            console.log("Masuk more");
+        let hasMore = true;
+        let pageNumber = 1;
+
+        while(hasMore) {
+            const timestamp = Math.floor(Date.now() / 1000);
+            const baseString = `${partner_id}${PATH}${timestamp}${access_token}${shop_id}`;
+            const sign = crypto.createHmac('sha256', partner_key)
+                .update(baseString)
+                .digest('hex');
+
+            const releaseTimeStart = Math.floor(new Date("2026-01-01") / 1000);
+            const releaseTimeEnd = Math.floor(new Date("2026-01-03") / 1000);
+            const params = new URLSearchParams({
+                partner_id: partner_id,
+                timestamp,
+                access_token: access_token,
+                shop_id: shop_id,
+                sign,
+                release_time_from: releaseTimeStart,
+                release_time_to: releaseTimeEnd,
+                page_size: 100,
+                page_number: pageNumber,
+            });
+
+            const fullUrl = `${HOST}${PATH}?${params.toString()}`;
+            console.log(`Hitting Dana Dilepas for ${brand}: `, fullUrl);
+
+            const response = await axios.get(fullUrl, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            
             let escrowList = response.data.response.escrow_list;
             
             console.log("Escrow list first 3: ");
             console.log(escrowList.slice(0, 3));
 
-            escrowList.forEach(c => {
-                count += 1;
-                print("Count: ", count);
-            })
+            count += escrowList.length;
+
+            hasMore = response.data.response.more;
+            pageNumber += 1;
         }
 
         console.log("[SHOPEE-WITHDRAWAL] Data count: ", count);
