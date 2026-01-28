387ae893f02cc5a1ab7f4ec95bf498f3b08a3539
Michael Chandra <michaelchandra@Michaels-MacBook-Air.local>
2026-01-28 15:50:00 +0700

    december fetch all brand

--- functions/walletTransactions.js
+++ functions/walletTransactions.js
@@ -20,8 +20,8 @@ async function fetchWalletTransaction(brand, partner_id, partner_key, access_tok
         let transactionContainer = [];
 
         while(hasMore) {
-            const createTimeFrom = Math.floor(new Date("2026-01-14") / 1000);
-            const createTimeTo = Math.floor(new Date("2026-01-19") / 1000);
+            const createTimeFrom = Math.floor(new Date("2025-12-01") / 1000);
+            const createTimeTo = Math.floor(new Date("2025-12-15") / 1000);
 
             console.log("Create time from: ", createTimeFrom);
             console.log("Create time to: ", createTimeTo);
