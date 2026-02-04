617cf2b993b13e4031c28d7514d34f5fad6ceb4c
Michael Chandra <michaelchandra@Michaels-MacBook-Air.local>
2026-02-04 10:01:38 +0700

    testing refresh tokens

--- functions/handleFinance.js
+++ functions/handleFinance.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import crypto from 'crypto';
 import axios from 'axios';
-
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+const secretClient = new SecretManagerServiceClient();
 // TODO:
 // 1. Bikin function getAccessToken from Secret Manager
 // 2. Bikin function refreshTokens, ambil refreshToken dari Secret Manager
@@ -17,15 +18,90 @@ import axios from 'axios';
 }
 */
 
-async function loadTokens() {
-
+const tiktokSecrets = {
+    "Eileen Grace": "projects/231801348950/secrets/eg-tiktok-tokens"
 }
 
-async function refreshTokens() {
-
+async function loadTokens(brand) {
+    const secretName = tiktokSecrets[brand] + "/versions/latest";
+    try {
+        const [version] = await secretClient.accessSecretVersion({
+            name: secretName
+        });
+        const data = version.payload.data.toString('UTF-8');
+        const tokens = JSON.parse(data);
+        console.log("[TIKTOK-SECRETS] Tokens loaded: ", tokens);
+        return tokens;
+    } catch (e) {
+        console.log("[TIKTOK-SECRETS] Error loading tokens for brand: ", brand);
+        console.log(e);
+    }
 }
 
-async function getShopCipher(brand) {
+async function saveTokens(brand, tokens) {
+    const parent = tiktokSecrets[brand];
+    const payload = Buffer.from(JSON.stringify(tokens, null, 2), 'UTF-8');
+
+    try {
+        const [newTokens] = await secretClient.addSecretVersion({
+            parent: parent,
+            payload: {
+                data: payload,
+            }
+        });
+
+        console.log("Saved Tiktok Tokens to Secret Manager on brand: ", brand);
+
+        const [prevTokens] = await secretClient.listSecretVersions({
+            parent: parent
+        });
+        
+        for(const prevToken of prevTokens) {
+            if(prevToken.name !== newTokens.name && prevToken.state !== 'DESTROYED') {
+                try {
+                    await secretClient.destroySecretVersion({
+                        name: prevToken.name
+                    })
+                } catch (destroyError) {
+                    console.error(`[TIKTOK-SECRETS] Failed to destroy version ${version.name}:`, destroyError);
+                }
+            }
+        }
+    } catch (e) {
+        console.log("[TIKTOK-SECRETS] Error saving tokens to Secret Manager: ", e);
+    }
+}
+
+// Tokens are exclusive per shop
+// Refresh token itself contains identity of the corresponding shop
+// Such is why it does not need shop_cipher or any other parameters. 
+
+async function refreshTokens(brand, refreshToken) {
+
+    const tokens = await loadTokens(brand);
+    const appKey = process.env.TIKTOK_PARTNER_APP_KEY;
+    const appSecret = process.env.TIKTOK_PARTNER_APP_SECRET;
+
+    const refreshUrl = "auth.tiktok-shops.com/api/v2/token/refresh";
+    const queryParams = "?" + "app_key=" + appKey + "&" + "app_secret=" + appSecret + "&" + "refresh_token=" + refreshToken + "&" + "grant_type=refresh_token";
+    const completeUrl = refreshUrl + queryParams;
+
+    try {   
+        const response = await axios.get(completeUrl);
+
+        let newAccessToken = response.data.data.access_token;
+        let newRefreshToken = response.data.data.refresh_token;
+
+        await saveTokens({
+            accessToken: newAccessToken, 
+            refreshToken: newRefreshToken
+        });
+    } catch (e) {
+        console.log("[TIKTOK-SECRETS] Error refreshing tokens: ", e);
+    }
+}
+
+async function getShopCipher(brand, accessToken) {
     try {
         const appKey = process.env.TIKTOK_PARTNER_APP_KEY;
         const appSecret = process.env.TIKTOK_PARTNER_APP_SECRET;
@@ -44,7 +120,7 @@ async function getShopCipher(brand) {
 
         const headers = {
             'content-type': 'application/json',
-            'x-tts-access-token': process.env.EG_TIKTOK_ACCESS_TOKEN,
+            'x-tts-access-token': accessToken,
         }
         const params = {
             app_key: process.env.TIKTOK_PARTNER_APP_KEY,
@@ -74,7 +150,14 @@ async function getShopCipher(brand) {
 }
 
 export async function handleFinance(brand) {
-    const shopCipher = await getShopCipher(brand);
+
+    const tokens = await loadTokens(brand);
+    let accessToken = tokens.accessToken;
+    let refreshToken = tokens.refreshToken;
+
+    await refreshTokens(brand, refreshToken);
+
+    const shopCipher = await getShopCipher(brand, accessToken);
     console.log("Shop cipher for brand: ", brand, ": ", shopCipher);
     // const shopCipher = await getShopCipher(brand);
 }
