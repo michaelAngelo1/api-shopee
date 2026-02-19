9291586d1557ec050d3e5c020e17faede3b932c8
Michael Chandra <michaelchandra@172-10-23-6.lightspeed.clmasc.sbcglobal.net>
2026-02-19 23:47:15 +0700

    specifier

--- functions/handleRealtime.js
+++ functions/handleRealtime.js
@@ -138,7 +138,7 @@ async function getOrderDetail(brand, batch, partner_id, partner_key, access_toke
 
                         const qty = item.model_quantity_purchased || 0;
                         totalGMV += (price * qty);
-                        console.log("Total GMV running total: ", totalGMV);
+                        console.log("Total GMV running total: ", totalGMV, " for brand: ", brand);
                     });
                 }
             });
