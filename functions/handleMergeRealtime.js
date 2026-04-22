870b327c6db80f79749aa80a15c1dea511de2e4f
Michael Chandra <michaelchandra@Michaels-MacBook-Air.local>
2026-04-22 09:34:34 +0700

    if no sales, leave as is

--- functions/handleMergeRealtime.js
+++ functions/handleMergeRealtime.js
@@ -61,19 +61,21 @@ export async function handleMergeRealtime(brand, marketplace, sales_value, order
             const utc7Time = new Date(now.getTime() + (7 * 60 * 60 * 1000));
             const formattedTimestamp = utc7Time.toISOString().replace('T', ' ').substring(0, 19);
             
-            row.assign({
-                'GMV': sales_value,
-                'Timestamp': formattedTimestamp,
-            });
-    
-            if (row._rawData.length > 4) {
-                row._rawData = row._rawData.slice(0, 4);
-            }
-    
-            await row.save();
+            if(sales_value > 0) {
+                row.assign({
+                    'GMV': sales_value,
+                    'Timestamp': formattedTimestamp,
+                });
+        
+                if (row._rawData.length > 4) {
+                    row._rawData = row._rawData.slice(0, 4);
+                }
+        
+                await row.save();
 
-            setTimeout(() => {
-            }, 3000)
+                setTimeout(() => {
+                }, 3000)
+            }
         }
 
     }
@@ -87,20 +89,22 @@ export async function handleMergeRealtime(brand, marketplace, sales_value, order
             const utc7Time = new Date(now.getTime() + (7 * 60 * 60 * 1000));
             const formattedTimestamp = utc7Time.toISOString().replace('T', ' ').substring(0, 19);
             
-            row.assign({
-                'GMV': sales_value,
-                'Orders': orders_count,
-                'Timestamp': formattedTimestamp,
-            });
-    
-            if (row._rawData.length > 5) {
-                row._rawData = row._rawData.slice(0, 5);
-            }
-    
-            await row.save();
+            if(sales_value > 0) {
+                row.assign({
+                    'GMV': sales_value,
+                    'Orders': orders_count,
+                    'Timestamp': formattedTimestamp,
+                });
+        
+                if (row._rawData.length > 5) {
+                    row._rawData = row._rawData.slice(0, 5);
+                }
+        
+                await row.save();
 
-            setTimeout(() => {
-            }, 3000)
+                setTimeout(() => {
+                }, 3000)
+            }
         }
 
     }
